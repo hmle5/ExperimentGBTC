b2555567a3c6dee64370e17a10bd92b999102e5c
psrajoria <psrajoria@outlook.com>
2025-04-10 14:11:19 +0200

    Fixed migrations

--- src/migrations/versions/f8283e1acab9_initial_migration.py
+++ src/migrations/versions/f8283e1acab9_initial_migration.py
@@ -1,8 +1,8 @@
-"""Initial migration
+"""initial migration
 
-Revision ID: 2124e3696613
+Revision ID: f8283e1acab9
 Revises: 
-Create Date: 2025-04-10 13:29:37.926765
+Create Date: 2025-04-10 14:08:15.114492
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2124e3696613'
+revision = 'f8283e1acab9'
 down_revision = None
 branch_labels = None
 depends_on = None
